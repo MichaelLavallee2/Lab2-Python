746eac4c5ee312fc2009b4fd5b1e78dda2da3571
Michael Lavallee <mike858123@gmail.com>
2020-09-11 03:54:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Author: Michael Lavallee mal6423@gmail.com
+# Collaborator:
+# Collaborator:
+# Collaborator:
+# Section: 
+# Breakout: 
+
+
+grade = float(input("Enter your CMPSC 131 grade: "))
+def conv(grade):
+  if grade > 93:
+    return "A"
+  elif grade > 90 and grade <= 93:
+    return "A-"
+  elif grade < 90 and grade >= 87:
+    return "B+"
+  elif grade < 87 and grade >= 83:
+    return "B"
+  elif grade < 83 and grade >= 80:
+    return "B-"
+  elif grade < 80 and grade >= 77:
+    return "C+"
+  elif grade < 77 and grade >= 70:
+    return "C"
+  elif grade < 70 and grade >= 60:
+    return "D"
+  elif grade < 60:
+    return "F"
+  else:
+    return "Error"
+
+print(f"Your letter grade for CMPSC 131 is: {conv(grade)}.")
